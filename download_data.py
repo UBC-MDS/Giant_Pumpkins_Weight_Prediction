57e744635507aeeb89d60800024b4b5495fc759f
Mahsa Sarafrazi <ma.sarafrazi@gmail.com>
2021-11-20 17:07:30 -0800

    url and local path options are added + Entries rows are skipped

--- download_data.py
+++ download_data.py
@@ -1,17 +1,25 @@
 # author : Mahsa Sarafrazi
 # date : 2021-11-19
 
-# Downloads a csv data from https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-10-19/pumpkins.csv
-# and save it to data/pumpkins_data.csv
+""" Downloads a csv data file from a url and save it to local file path as csv format.
 
+Usage: download_data.py --url=<url> --out_file=<out_file>
+
+Options:
+--url =<url>               the path in which the data is available  (must be in standard csv format)
+--out_file=<out_file>      the path where to localy write the file
+"""
 
 import pandas as pd
 import os
+from docopt import docopt
 
-def main():
-    url = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-10-19/pumpkins.csv"
-    out_file = "data/pumpkins_data.csv"
-    data = pd.read_csv(url)
+opt = docopt(__doc__)
+
+def main(url, out_file):
+    
+    data = pd.read_csv(url,skiprows= [292, 496, 2178, 2330, 2620, 2894, 3225, 3411, 5312, 5505, 5800, 6073, 6393, 6613, 8594, 8801, 9116, 9392, 9727, 9984, 11783, 11990, 12317, 12610, 12939, 13140, 14883, 15111, 15476, 15755, 16072, 16292, 18198, 18378, 18745, 19060, 19361, 19616, 21500, 21698, 22150, 22441, 22688, 22915, 24485, 24645, 25029, 25301, 25585, 25780, 27279, 27440, 27811, 28065])
+    
     try:
         data.to_csv(out_file, index = False)
     except:
@@ -19,4 +27,4 @@ def main():
          data.to_csv(out_file, index = False)
 
 if __name__ == "__main__":
-    main()
+    main(opt['--url'],opt['--out_file'])
