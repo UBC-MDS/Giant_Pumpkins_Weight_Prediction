6ece86efec00131314c501f473a21da35bd2be1e
Mahsa Sarafrazi <ma.sarafrazi@gmail.com>
2021-11-20 20:06:21 -0800

    update code for skipping rows and reading numbers

--- src/script/download_data.py
+++ src/script/download_data.py
@@ -17,14 +17,15 @@ from docopt import docopt
 opt = docopt(__doc__)
 
 def main(url, out_file):
-    
-    data = pd.read_csv(url,skiprows= [292, 496, 2178, 2330, 2620, 2894, 3225, 3411, 5312, 5505, 5800, 6073, 6393, 6613, 8594, 8801, 9116, 9392, 9727, 9984, 11783, 11990, 12317, 12610, 12939, 13140, 14883, 15111, 15476, 15755, 16072, 16292, 18198, 18378, 18745, 19060, 19361, 19616, 21500, 21698, 22150, 22441, 22688, 22915, 24485, 24645, 25029, 25301, 25585, 25780, 27279, 27440, 27811, 28065])
-    
+
+    pumpkins_df= pd.read_csv(url, thousands=",")
+    pumpkins_df = pumpkins_df[-pumpkins_df["place"].str.contains("Entries")]
+
     try:
-        data.to_csv(out_file, index = False)
+        pumpkins_df.to_csv(out_file, index = False)
     except:
         os.makedirs(os.path.dirname(out_file))
-        data.to_csv(out_file, index = False)
+        pumpkins_df.to_csv(out_file, index = False)
 
 if __name__ == "__main__":
     main(opt['--url'],opt['--out_file'])
